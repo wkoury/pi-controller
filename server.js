aa8aff36b68861f043a3511262b25e5f5703c61f
Will Koury <wkoury@hey.com>
2021-04-07 11:09:36 -0400

    First attempt at CPU temp

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { exec } = require("child_process");
+const { promiseImpl } = require("ejs");
 
 const PORT = 8888;
 
@@ -10,8 +11,35 @@ app.set("view options", { layout: false } );
 
 app.use(express.static(__dirname + '/public'));
 
+function getCPUTemperature(){
+	return new Promise((resolve) => {
+		exec("cat /sys/class/thermal/thermal_zone0/temp", (error, stdout, stderr) => {
+			
+			if(error){
+				resolve({
+					"celsius": "NaN",
+					"fahrenheit": "NaN"
+				});
+			}
+
+			const celsius = stdout / 1000;
+			const fahrenheit = celsius * (9/5) + 32;
+			
+			resolve({
+				"celsius": celsius,
+				"fahrenheit": fahrenheit
+			});
+		});
+	});
+}
+
 app.get("/", (req, res) => {
-	res.render("index");
+	getCPUTemperature().then(temps => {
+		const { fahrenheit } = temps;
+		res.render("index", {
+			fahrenheit: fahrenheit
+		});
+	});
 });
 
 app.post("/rs", (req, res) => {
